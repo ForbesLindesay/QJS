0499c7931918a20fd22dcae127cd92f6d950f264
Forbes Lindesay <forbes.lindesay@red-gate.com>
2012-08-13 17:36:26 +0100

    Handle var statements properly
    todo: fix conditionals in while statements, add support for for, with,
    do...while, try...catch, switch...case...

--- test/fixtures/while.js
+++ test/fixtures/while.js
@@ -1,19 +1,17 @@
 require('../../').compile(module, function () {
-    module.exports.inBody = function foo() {
+    module.exports.inBody = function () {
         var i = 0;
         while ((++i) < 2) {
             await(Q.resolve(1));
-                    console.log(' i is ');
-        console.log(i);
         }
-        require('../../lib/compile').debug(foo);
-        console.log(' i is ');
-        console.log(i);
         return i;
     };
-    module.exports.inCondition = function () {
+    module.exports.inCondition = function foo() {
+        require('../../lib/compile').debug(foo);
         var i = 0;
         while (await(Q.resolve(++i)) < 2) {
+            console.log(i);
+            return 'todo: fix infinite loop';
         }
         return i;
     };
