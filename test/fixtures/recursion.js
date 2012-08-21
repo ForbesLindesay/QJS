e1734bebc2c04ad3ce619e8a2633ee8cfe94a655
Forbes Lindesay <forbes.lindesay@red-gate.com>
2012-08-21 10:47:06 +0100

    Fixed recursion for functions with await in them

--- test/fixtures/recursion.js
+++ test/fixtures/recursion.js
@@ -1,7 +1,14 @@
 require('../../').compile(module, function () {
     var Q = require('q');
     module.exports.fact = function fact(n) {
-        var next = Q.delay(n - 1, 1);
+        if (n === 0) return 1;
+        var next = await(Q.delay(n - 1, 1));
         return await(fact(next)) * n;
+    };
+    module.exports.all = function all() {
+        for (var i = 0; i < arguments.length; i++) {
+            arguments[i] = await(arguments[i]);
+        }
+        return arguments;
     };
 });
